982ea26bdbcbfc3166160222cc29d12a39b7524f
Oto Siradze <osiradze@raizomat.com>
2022-09-01 11:36:10 +0400

    made hilt provide methods singleton

--- movies/src/main/java/com/siradze/movies/di/MoviesNetworkModule.kt
+++ movies/src/main/java/com/siradze/movies/di/MoviesNetworkModule.kt
@@ -1,37 +1,32 @@
 package com.siradze.movies.di
 
 import android.content.Context
-import com.google.gson.GsonBuilder
+
 import com.siradze.movies.data.api.ApiStrings
 import com.siradze.movies.data.api.interceptors.ApiKeyInterceptor
 import com.siradze.movies.data.api.interceptors.ErrorResponseInterceptor
 import com.siradze.movies.data.api.interceptors.NetworkConnectionInterceptor
-import com.siradze.movies.movieDetails.data.api.MovieDetailsDeserializer
-import com.siradze.movies.movieDetails.data.api.MovieDetailsResponse
-import com.siradze.movies.movieDetails.data.api.SimilarMoviesDeserializer
-import com.siradze.movies.movieDetails.data.api.SimilarMoviesResponse
-import com.siradze.movies.movies.data.api.MoviesDeserializer
-import com.siradze.movies.movies.data.api.MoviesResponse
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MoviesNetworkModule {
 
+    @Singleton
     @Provides
     @Named("base_url")
     fun provideBaseUrl() : String{
         return ApiStrings.base_url
     }
 
+    @Singleton
     @Provides
     fun providesHttpClient(@ApplicationContext appContext: Context) : OkHttpClient {
         return OkHttpClient.Builder().addInterceptor { chain ->
---
+++ movies/src/main/java/com/siradze/movies/movieDetails/di/MovieDetailsModule.kt
@@ -0,0 +1,53 @@
+package com.siradze.movies.movieDetails.di
+import com.google.gson.GsonBuilder
+import com.siradze.movies.movieDetails.data.MovieDetailsRepository
+import com.siradze.movies.movieDetails.data.MovieDetailsRepositoryImp
+import com.siradze.movies.movieDetails.data.api.*
+import com.siradze.movies.movieDetails.data.dataSource.MovieDetailsDataSource
+import com.siradze.movies.movieDetails.data.dataSource.MovieDetailsDataSourceImp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MovieDetailsModule {
+
+    @Singleton
+    @Provides
+    fun providesMovieDetailsRepository(dataSource: MovieDetailsDataSource) : MovieDetailsRepository {
+        return MovieDetailsRepositoryImp(dataSource)
+    }
+    @Singleton
+    @Provides
+    fun providesMovieDetailsDataSource(api: MovieDetailsApi) : MovieDetailsDataSource {
+        return MovieDetailsDataSourceImp(api)
+    }
+    @Singleton
+    @Provides
+    fun provideMovieDetailsApi(@Named("movieDetails_retrofit") retrofit: Retrofit) : MovieDetailsApi {
+       return retrofit.create(MovieDetailsApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    @Named("movieDetails_retrofit")
+    fun providesRetrofit(@Named("base_url") baseUrl: String, okHttpClient: OkHttpClient): Retrofit {
+        val customGson = GsonBuilder()
+            .registerTypeAdapter(MovieDetailsResponse::class.java, MovieDetailsDeserializer())
+            .registerTypeAdapter(SimilarMoviesResponse::class.java, SimilarMoviesDeserializer())
+            .create()
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(customGson))
+            .build()
+    }
+}
